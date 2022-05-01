d3598158d3fe6fd3de36a78fd87753143f12b194
brk-a <fnjakai@outlook.com>
2022-05-01 02:38:29 -0700

    Task2 & 3: try v 3.0

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+
+'''
+Deletion-resilient hypermedia pagination
+'''
+
+import csv
+import math
+from typing import List, Dict, Any
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int\
+                        = None, page_size: int = 10) -> Dict:
+        """ implements hypermedia pagination
+        """
+        assert type(index) == int
+        assert type(page_size) == int
+        csv_file = self.indexed_dataset()
+        csv_size = len(csv_file)
+        assert 0 <= index < csv_size
+        data = []
+        next_page = index
+        for _ in range(page_size):
+            while not csv_file.get(next_page):
+                next_page += 1
+            data.append(csv_file.get(next_page))
+            next_page += 1
+        return {
+            "index": index,
+            "page_size": page_size,
+            "next_index": next_page,
+            "data": data,
+            "page_size": page_size
+        }
+
+
+if __name__ == '__main__':
+    Server = __import__('3-hypermedia_del_pagination').Server
+
+    server = Server()
+
+    server.indexed_dataset()
+
+    try:
+        server.get_hyper_index(300000, 100)
+    except AssertionError:
+        print("AssertionError raised when out of range")
+
+    index = 3
+    page_size = 2
+
+    print("Nb items: {}".format(len(server._Server__indexed_dataset)))
+
+    # 1- request first index
+    res = server.get_hyper_index(index, page_size)
+    print(res)
+
+    # 2- request next index
+    print(server.get_hyper_index(res.get('next_index'), page_size))
+
+    # 3- remove the first index
+    del server._Server__indexed_dataset[res.get('index')]
+    print("Nb items: {}".format(len(server._Server__indexed_dataset)))
+
+    # 4- request again the initial index ->
+    #the first data retreives is not the same as the first request
+    print(server.get_hyper_index(index, page_size))
+
+    # 5- request again initial next index ->
+    #same data page as the request 2-
+    print(server.get_hyper_index(res.get('next_index'), page_size))
