ad9767695b1bb46981a1d392c047db6f9d8c1076
brk-a <fnjakai@outlook.com>
2022-05-04 00:22:33 -0700

    Task 4: try v 1.2

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+
+'''
+MRU Caching
+'''
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """Implements the MRU caching algorithm """
+    def __init__(self):
+        """Initialises the class instance. """
+        super().__init__()
+        self.keys = []
+
+    def put(self, key, item):
+        """Adds an item to the dictionary """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if key not in self.keys:
+                self.keys.append(key)
+            else:
+                self.keys.append(self.keys.pop(self.keys.index(key)))
+            if len(self.keys) > BaseCaching.MAX_ITEMS:
+                discard = self.keys.pop(-2)
+                del self.cache_data[discard]
+                print('DISCARD: {:s}'.format(discard))
+
+    def get(self, key):
+        """Return item stored by the key else returns None """
+        if key is not None and key in self.cache_data:
+            self.keys.append(self.keys.pop(self.keys.index(key)))
+            return self.cache_data[key]
+        return None
+
+if __name__ == '__main__':
+    MRUCache = __import__('4-mru_cache').MRUCache
+
+    my_cache = MRUCache()
+    my_cache.put("A", "Hello")
+    my_cache.put("B", "World")
+    my_cache.put("C", "Holberton")
+    my_cache.put("D", "School")
+    my_cache.print_cache()
+    print(my_cache.get("B"))
+    my_cache.put("E", "Battery")
+    my_cache.print_cache()
+    my_cache.put("C", "Street")
+    my_cache.print_cache()
+    print(my_cache.get("A"))
+    print(my_cache.get("B"))
+    print(my_cache.get("C"))
+    my_cache.put("F", "Mission")
+    my_cache.print_cache()
+    my_cache.put("G", "San Francisco")
+    my_cache.print_cache()
+    my_cache.put("H", "H")
+    my_cache.print_cache()
+    my_cache.put("I", "I")
+    my_cache.print_cache()
+    my_cache.put("J", "J")
+    my_cache.print_cache()
+    my_cache.put("K", "K")
+    my_cache.print_cache()
